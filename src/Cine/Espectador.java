2876f16913413f0a7be3e52dc6b201bfa0604f9a
Gabriel21 <carrilloricogabrieljesus@gmail.com>
2024-10-06 20:53:40 -0500

    Informacion, datos, metodo Random, matrices, logica (mostrar datos)(no mostrar ya seleccionados) condiciones (mayor de 18) (mas de 10000 para entrar) - falta (mostrar los asientos disponibles - mostrar los asientos no disponibles)

--- src/Cine/Espectador.java
+++ src/Cine/Espectador.java
@@ -1,31 +1,55 @@
 package Cine;
 
 
-public abstract class Espectador {
+public class Espectador {
 
     String Nom[][] = {
-            {"gabriel", "jesus","carrillo","rico"},
-            {"lesly", "alexandra", "villacob", "gelvez"},
-            {"Jose", "aroldo", "ortega", "ortiz"}
+            {"gabriel", "jesus","carrillo","rico", "gabriel", "jesus","carrillo","rico", "batman"},
+            {"lesly", "alexandra", "villacob", "gelvez", "gabriel", "jesus","carrillo","rico","batman" },
+            {"Jose", "aroldo", "ortega", "ortiz", "gabriel", "jesus","carrillo","rico", "batman"},
+            {"Jose", "aroldo", "ortega", "ortiz", "gabriel", "jesus","carrillo","rico", "batman"},
+            {"Jose", "aroldo", "ortega", "ortiz", "gabriel", "jesus","carrillo","rico", "batman"},
+            {"Jose", "aroldo", "ortega", "ortiz", "gabriel", "jesus","carrillo","rico", "batman"},
+            {"Jose", "aroldo", "ortega", "ortiz", "gabriel", "jesus","carrillo","rico", "batman"},
+            {"Jose", "aroldo", "ortega", "ortiz", "gabriel", "jesus","carrillo","rico", "batman"},
+            {"Jose", "aroldo", "ortega", "ortiz", "gabriel", "jesus","carrillo","rico", "batman"}
     } ;
 
     String puestos[][] = {
-            {"1A", "1B", "1C" , "1D"},
-            {"2A", "2B", "2C" , "2D"},
-            {"3A", "3B", "3C", "3D" }
+            {"1A", "1B", "1C", "1D" ,"1E", "1F", "1G" , "1H", "1I"},
+            {"2A", "2B", "2C", "2D" ,"2E", "2F", "2G" , "2H", "2I"},
+            {"3A", "3B", "3C", "3D" ,"3E", "3F", "3G" , "3H", "3I"},
+            {"4A", "4B", "4C", "4D" ,"4E", "4F", "4G" , "4H", "4I"},
+            {"5A", "5B", "5C", "5D" ,"5E", "5F", "5G" , "5H", "5I"},
+            {"6A", "6B", "6C", "6D" ,"6E", "6F", "6G" , "6H", "6I"},
+            {"7A", "7B", "7C", "7D" ,"7E", "7F", "7G" , "7H", "7I"},
+            {"8A", "8B", "8C", "8D" ,"8E", "8F", "8G" , "8H", "8I"},
+            {"9A", "9B", "9C", "9D" ,"9E", "9F", "9G" , "9H", "9I"}
 
     } ;
 
     int edades[][] = {
-            {10, 12, 16 , 18},
-            {20, 22, 12 , 17},
-            {30, 32, 10, 33 }
+            {10, 12, 16 , 18, 10, 12, 16 , 18, 9 },
+            {20, 22, 12 , 17, 10, 12, 16 , 18, 9},
+            {20, 22, 12 , 17, 10, 12, 16 , 18, 9},
+            {20, 22, 12 , 17, 10, 12, 16 , 18, 9},
+            {20, 22, 12 , 17, 10, 12, 16 , 18, 9},
+            {20, 22, 12 , 17, 10, 12, 16 , 18, 9},
+            {20, 22, 12 , 17, 10, 12, 16 , 18, 9},
+            {20, 22, 12 , 17, 10, 12, 16 , 18, 9},
+            {30, 32, 10, 33,  10, 12, 16 , 18, 9 }
 
     } ;
     int plata[][] = {
-            {5000, 2000, 10000 , 15555},
-            {20000, 1500, 3300 , 50000},
-            {22000, 12000, 15000, 30 }
+            {5000, 2000, 10000 , 15555, 5000, 2000, 10000 , 15555, 15200},
+            {5000, 2000, 10000 , 15555, 5000, 2000, 10000 , 15555, 15200},
+            {5000, 2000, 10000 , 15555, 5000, 2000, 10000 , 15555, 15200},
+            {22000, 12000, 15000, 30,  5000, 2000, 10000 , 15555, 15200},
+            {20000, 1500, 3300 , 50000,  5000, 2000, 10000 , 15555, 15200},
+            {20000, 1500, 3300 , 50000,  5000, 2000, 10000 , 15555, 15200},
+            {20000, 1500, 3300 , 50000,  5000, 2000, 10000 , 15555, 15200},
+            {20000, 1500, 3300 , 50000,  5000, 2000, 10000 , 15555, 15200},
+            {22000, 12000, 15000, 30,  5000, 2000, 10000 , 15555, 15200}
 
     } ;
 
--- src/cinemajava/CinemaJava.java
+++ src/cinemajava/CinemaJava.java
@@ -1,10 +1,85 @@
 package cinemajava;
 
 import Cine.Espectador;
+import Cine.Peliculas;
+
+import java.util.Scanner;
 
 public class CinemaJava {
+
     public static void main(String[] args) {
 
+        int valor = 10000;
+        Scanner sc = new Scanner(System.in);
+        Espectador espectador = new Espectador();
+        Peliculas pelis = new Peliculas("Como programar y no morir 2", "Gabriel C", 18, 10000);
+
+        boolean continuar = true;
+
+       mainLoop: while (continuar) {
+           int numero = (int) ((Math.random() * 4) + 1);
+           int numero2 = (int) ((Math.random() * 4) + 1);
+            for (int i = numero; i < 9; i++) {
+                for (int j = numero2; j < 9; j++) {
+
+                    if (i < espectador.getNom().length && j < espectador.getPuestos().length) {
+
+                        System.out.println("nombre = " + espectador.getNom()[i][j]);
+                        System.out.println("edad = " + espectador.getEdades()[i][j]);
+                        System.out.println("Plata = " + espectador.getPlata()[i][j]);
+
+                            System.out.println("PUESTO DISPONIBLE = " + espectador.getPuestos()[i][j]);
+
+                        if (espectador.getPuestos()[i][j].equals("0")) {
+                            espectador.getPuestos()[i][j] = espectador.getPuestos()[numero2][numero];
+                            continue;
+                        }
+
+
+                                System.out.println("""
+                                        ¿ACEPTAR PUESTO?
+                                        1. SI
+                                        2. NO
+                                        3.SALIR""");
+
+                                int opc = sc.nextInt();
+                                switch (opc) {
+
+                                    case 1:
+                                        if (espectador.getEdades()[i][j] < 18 || espectador.getPlata()[i][j] < 10000) {
+                                            if (espectador.getEdades()[i][j] < 18) {
+                                                System.out.println("No tienes edad suficiente para ver esta película");
+                                            }
+                                            if (espectador.getPlata()[i][j] < 10000) {
+                                                System.out.println("No tienes dinero suficiente para ver esta película");
+                                            }
+                                            System.out.println(" ");
+                                            System.out.println("Pasando al siguiente Usuario...\n");
+                                            continue;
+                                        }
+                                        espectador.getPuestos()[i][j] = "0";
+                                        System.out.println("PUESTO ASIGNADO");
+                                    case 2:
+                                        continue;
+                                    case 3:
+                                        System.out.println("PELICULA = " + pelis.getTitulo());
+                                        System.out.println("DURACION = " + pelis.getDuracion());
+                                        System.out.println("AUTOR = " + pelis.getAutor());
+                                        continuar = false;
+                                        break mainLoop;
+                                    default:
+                                        System.out.println("OPCION INVALIDA");
+                                        break;
+                        }
+                    }
+                }
+
+            }
+
+        }
+        System.out.println("Gracias por ver");
 
     }
 }
+
+
